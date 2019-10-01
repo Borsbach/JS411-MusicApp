2b4bb87df79b93ffe6461abdd4b5b9af323b6c1c
Alex Borsbach <alexborsbach@Alexs-MacBook-Pro.local>
2019-09-30 21:03:47 -0500

    functionality working

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,128 +1,167 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-  Card, 
-  CardContent, 
-  Typography, 
-  CardActions, 
-  Switch, 
-  Slider, 
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Switch,
+  Slider,
   MenuItem,
   Select,
-  FormControl,
-} from '@material-ui/core'
-import Appbar from './Appbar'
+  FormControl
+} from "@material-ui/core";
+import Appbar from "./Appbar";
 
 class Dashboard extends Component {
   state = {
-      online: false,
-      volume: [],
-      quality: [],
-      notifications: [],
-  }
+    online: false,
+    volume: 50,
+    quality: 2,
+    notifications: {}
+  };
 
   componentDidUpdate(prevProps, prevState) {
-    if (this.state.quality !==prevState.quality) {
-      this.setState(this.state.quality)
+    if (this.state.quality !== prevState.quality) {
+      // set the quality message in this.state.notifications
+      const notes = this.state.notifications;
+      if (this.state.quality === 1) {
+        notes['quality'] = 'the quality is low'
+      } else {
+        notes['quality'] = ''
+      }
+      this.setState({ notifications: notes });
+    }
+    if (this.state.online !== prevState.online) {
+      // set the online message in this.state.notifications
+      const notes = this.state.notifications;
+      if (this.state.online) {
+        notes['online'] = 'Online'
+      } else {
+        notes['online'] = ''
+      }
+      this.setState({ notifications: notes });
+    }
+    if (this.state.volume !== prevState.volume) {
+      // set the volume message in this.state.notifications
+      const notes = this.state.notifications;
+      if (this.state.volume >= 80) {
+        notes['volume'] = 'too high'
+      } else {
+        notes['volume'] = ''
+      }
+      this.setState({ notifications: notes });
     }
   }
 
-
-  handleClick = () => {
-    console.log('Button online clicked')
-    this.setState({ online: !this.state.online })
+  handleVolumeChange = (event, value) => {
+    this.setState({ volume: value })
   }
 
-  render() { 
+  handleChange = event => {
+    this.setState({ quality: event.target.value });
+  };
+
+  handleClick = () => {
+    console.log("Button online clicked");
+    this.setState({ online: !this.state.online });
+  };
+
+  render() {
     return (
       <div>
         <Appbar />
         <h1>Welcome Alex!</h1>
         <br />
-        <div className='container1'>
-        <Card className='card1'>
-          <CardContent>
-            <Typography variant="h5" component="h5">
-              Online Mode
-            </Typography>
-            <Typography variant="body2" component="p">
-              Is this application connected
-            <br />
-            to the internet?
-            </Typography>
+        <div className="container1">
+          <Card className="card1">
+            <CardContent>
+              <Typography variant="h5" component="h5">
+                Online Mode
+              </Typography>
+              <Typography variant="body2" component="p">
+                Is this application connected
+                <br />
+                to the internet?
+              </Typography>
             </CardContent>
             <CardActions>
-            <Switch
-            onClick={this.handleClick}{...this.state.online}
-            // checked={state.checkedA}
-            // onChange={handleChange('checkedA')}
-            value="checkedA"
-            inputProps={{ 'aria-label': 'secondary checkbox' }}
-            />
+              <Switch
+                onClick={this.handleClick}
+                {...this.state.online}
+                // checked={state.checkedA}
+                // onChange={handleChange('checkedA')}
+                value="checkedA"
+                inputProps={{ "aria-label": "secondary checkbox" }}
+              />
             </CardActions>
           </Card>
-          <Card className='card2'>
-          <CardContent>
-            <Typography variant="h5" component="h5">
-              Master Volume
-            </Typography>
-            <Typography variant="body2" component="p">
-              Overrides all other sound
-            <br />
-            settings in this application
-            </Typography>
+          <br />
+          <Card className="card2">
+            <CardContent>
+              <Typography variant="h5" component="h5">
+                Master Volume
+              </Typography>
+              <Typography variant="body2" component="p">
+                Overrides all other sound
+                <br />
+                settings in this application
+              </Typography>
             </CardContent>
             <CardActions>
-            {/* <Typography id="discrete-slider" gutterBottom>
-        Temperature
-      </Typography> */}
-      <Slider onChange
-        defaultValue={30}
-        // getAriaValueText={valuetext}
-        aria-labelledby="discrete-slider"
-        valueLabelDisplay="auto"
-        step={10}
-        marks
-        min={0}
-        max={100}
-        />
-          </CardActions>
+              <Slider
+                onChange={this.handleVolumeChange}
+                value={this.state.volume}
+                // defaultValue={30}
+                // getAriaValueText={valuetext}
+                aria-labelledby="discrete-slider"
+                valueLabelDisplay="auto"
+                step={10}
+                marks
+                min={0}
+                max={100}
+              />
+            </CardActions>
           </Card>
-          <Card className='card3'>
-          <CardContent>
-            <Typography variant="h5" component="h5">
-              Sound Quality
-            </Typography>
-            <Typography variant="body2" component="p">
-              Manually control the music
-            <br />
-              quality in event of poor
-            <br />
-              connection   
-            </Typography>
+          <br />
+          <Card className="card3">
+            <CardContent>
+              <Typography variant="h5" component="h5">
+                Sound Quality
+              </Typography>
+              <Typography variant="body2" component="p">
+                Manually control the music
+                <br />
+                quality in event of poor
+                <br />
+                connection
+              </Typography>
             </CardContent>
             <CardActions>
-            <FormControl className='Form'>
-          <Select
-          quality={this.state.quality}
-          // onChange={handleChange}
-          displayEmpty
-          name=''
-          // className={classes.selectEmpty}
-          >
-          <MenuItem quality={1}>Low</MenuItem>
-          <MenuItem quality={2}>Normal</MenuItem>
-          <MenuItem quality={3}>High</MenuItem>
-          </Select>
-          </FormControl>
-          </CardActions>
+              <FormControl className="Form">
+                <Select
+                  value={this.state.quality}
+                  onChange={this.handleChange}
+                  displayEmpty
+                  name=""
+                  // className={classes.selectEmpty}
+                >
+                  <MenuItem value={1}>Low</MenuItem>
+                  <MenuItem value={2}>Normal</MenuItem>
+                  <MenuItem value={3}>High</MenuItem>
+                </Select>
+              </FormControl>
+            </CardActions>
           </Card>
         </div>
-        <div>
+        <div className="notifications">
           <h2>System Notifications:</h2>
-          Your application is {this.state.online ? 'online' : 'offline'}. You {this.state.online ? 'are' : 'are not'} able to share or stream music to other devices.
+          {/* <p>Your application is {this.state.online ? 'online' : 'offline'}. You {this.state.online ? 'are' : 'are not'} able to share or stream music to other devices.</p> */}
+          {Object.keys(this.state.notifications).map(n => {
+            return <p>{this.state.notifications[n]}</p>;
+          })}
         </div>
       </div>
-    ) 
+    );
   }
 }
 
